23c7df80473ce25bda4a1f4ca67c092d1c7b8685
Kwstas <deekwstaz@gmail.com>
2021-03-07 23:21:52 +0200

    Implemented Firestore SignUpWithEmailAndPasssword method

--- src/App.js
+++ src/App.js
@@ -8,15 +8,28 @@ import ShopPage from "./pages/shop/Shop";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/SignInAndSignUp";
 
 // Auth
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 function App() {
-  const [user, setUser] = useState({ currentUser: null });
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
-    const unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      setUser({ currentUser: user });
+    const unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          setUser({
+            id: snapShot.id,
+            ...snapShot.data()
+          });
+        });
+      }
+
+      setUser(userAuth); // equivalent to null if !userAuth
     });
+
+    console.log(user);
 
     return () => {
       unsubscribeFromAuth();
